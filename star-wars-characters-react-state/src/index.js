909fd1112e1064ecaf7419fdb4ed2132a91de339
Dan Pirozhok <onejump9@gmail.com>
2021-05-04 16:27:08 +0300

    Data Fetching, response, loading and error. Fetching with custom hook

---
+++ star-wars-characters-react-state/src/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import CharacterList from './CharacterList';
+
+import dummyData from './dummy-data';
+import endpoint from './endpoint';
+
+import './styles.scss';
+
+const useFetch = (url) => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setResponse(null);
+    setError(null);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setLoading(false);
+        setResponse(response);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error);
+      });
+  }, []);
+
+  return [response, loading, error];
+};
+
+const Application = () => {
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+  const characters = (response && response.characters) || [];
+
+  return (
+    <div className="Application">
+      <header>
+        <h1>Star Wars Characters</h1>
+      </header>
+      <main>
+        <section className="sidebar">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p>{error.message}</p>}
+        </section>
+      </main>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Router>
+    <Application />
+  </Router>,
+  rootElement,
+);
